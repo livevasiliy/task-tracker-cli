6b7fa38018ea08d633b091d05c3730ef761d6c15
Vasiliy <vasiliy.mv.obzor@gmail.com>
2025-03-12 10:56:07 +0300

    build native app for OS

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
@@ -12,6 +13,18 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass.set("com.example.App")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.example.App"
+    }
+
+    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 }
 
 tasks.test {
